5fdea59256a3f0ab48c3a6014e979b4317125225
BolunZhu <1870759078@qq.com>
2017-04-01 19:19:00 +0800

    hyanghui triangle 1 right 2 AC

---
+++ hy2.c
@@ -0,0 +1,30 @@
+include<stdio.h>
+int yh(int x,int y);
+int main (void)
+{
+    int n;
+    while (scanf("%d",&n)==1&&n>0) {
+        for (int i=1; i<=n; i++) {
+            for (int j=0; j<2*(n-i); j++) {
+                printf(" ");
+            }
+            for (int j=0; j<i; j++) {
+                if (j==0) {
+                    printf("%d",yh(i-1, j));
+                }
+                else{
+                    printf("%4d",yh(i-1,j));
+                }
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+int yh(int x,int y){
+    if (y==0) {
+        return 1;
+    }
+    return (x-y+1)*yh(x, y-1)/y;
+}
