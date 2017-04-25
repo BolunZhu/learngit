8a6f0387520e1726d4b9c5c420a9331ff9532954
BolunZhu <1870759078@qq.com>
2017-04-25 21:04:20 +0800

    c

--- cwork#41.c
+++ cwork#41.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    int mat[4][3];
+    int mat[3][4];
     for (int i=0; i<3; i++) {
         for (int j=0; j<4; j++) {
-            scanf("%d",&mat[j][i]);
+            scanf("%d",&mat[i][j]);
         }
     }
     for (int i=0; i<3; i++) {
         for (int j=0; j<4; j++) {
-            printf("%5d",mat[j][i]);
+            printf("%5d",mat[i][j]);
         }
         printf("\n");
     }
     printf("\n");
     for (int i=0; i<4; i++) {
         for (int j=0; j<3; j++) {
-            printf("%5d",mat[i][j]);
+            printf("%5d",mat[j][i]);
         }
         printf("\n");
     }
