baf52aac8376bd258e0efc519e8be84915f23590
BolunZhu <1870759078@qq.com>
2017-05-08 19:40:51 +0800

    s

---
+++ cwork#90.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int num[10]={0};
+    int *p[10]={NULL};
+    do {
+        for (int i=0; i<10; i++) {
+            scanf("%d",&num[i]);
+            p[i]=&num[i];
+        }
+        for (int i=0; i<10; i++) {
+            for (int j=9, *temp=p[i]; j>0; j--) {
+                if (*p[j]<*p[j-1]) {
+                    temp=p[j];
+                    p[j]=p[j-1];
+                    p[j-1]=temp;
+                }
+            }
+        }
+        for (int i=0; i<9; i++) {
+            printf("%d->",*p[i]);
+        }
+        printf("%d\n",*p[9]);
+    } while (getchar()!=EOF);
+    return 0;
+}
